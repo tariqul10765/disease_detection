f93fb097f043a8c2a1cb6ffec6b004502b47febb
tariqul10765 <tariqul10765@gmail.com>
2021-12-10 19:14:11 +0600

    heroku deploy

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ app.post('/disease', (req, res) => {
 
         disease.stdout.on('data', function (data) {
             result += data.toString();
-            // const diseaseSplit = result.split(`1.0\r\n41\r\n`)[1];
+            const diseaseSplit = result.split(`1.0\r\n41\r\n`)[1];
 
-            // res.json({
-            //     message: 'Disease get successfully',
-            //     data: diseaseSplit
-            // })
+            res.json({
+                message: 'Disease get successfully',
+                data: diseaseSplit
+            })
         });
         disease.stderr.on('data', (data) => {
             result += data.toString();
